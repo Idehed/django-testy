104786273948fc080daba46fe9e415a90df8fd5c
Idehed anton <anton.idehed@telia.com>
2024-04-07 11:28:03 +0000

    false

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ if os.path.exists('env.py'):
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-idehed-djangotesty-u7idq6gx2t1.ws-eu110.gitpod.io','.herokuapp.com']
 
